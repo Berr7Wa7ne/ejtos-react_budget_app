655698e9a842d77d275f845cbd33111896edc38e
Berr7Wa7ne <cosyberry3@gmail.com>
2024-02-05 04:04:52 -0500

    The currency prefix has been updated

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,30 +1,24 @@
+// Budget.js
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-  const { budget, dispatch, expenses } = useContext(AppContext);
+  const { budget, dispatch, expenses, currency } = useContext(AppContext);
   const [editable, setEditable] = useState(false);
   const [newBudget, setNewBudget] = useState(budget);
   const upperLimit = 20000;
 
   const totalSpending = expenses.reduce((total, item) => total + item.cost, 0);
 
-  const handleIncrease = () => {
-    setNewBudget((prevBudget) => Math.min(prevBudget + 10, upperLimit));
-  };
-
-  const handleDecrease = () => {
-    setNewBudget((prevBudget) => Math.max(prevBudget - 10, 0));
-  };
-
   const handleSave = () => {
     if (newBudget > upperLimit) {
       // Display error message and do not update the budget
       alert("Error: Budget cannot exceed £20,000");
-    }else if (newBudget < totalSpending) {
-        alert("Error: Budget cannot be lower than spending");
-    }else{
-        // Include the payload field with the new budget value
+    } else if (newBudget < totalSpending) {
+      // Display error message as the budget cannot be lower than spending
+      alert("Error: Budget cannot be lower than the total spending");
+    } else {
+      // Include the payload field with the new budget value
       dispatch({
         type: 'UPDATE_BUDGET',
         payload: newBudget, // Include the new budget value in the payload
@@ -35,22 +29,33 @@ const Budget = () => {
 
   return (
     <div className='alert alert-secondary'>
-      <span>Budget: £{budget}</span>
-
+      <label>Budget:</label>
       {editable ? (
         <div>
+          <label>{currency}</label>
           <input
             type="number"
             step="10"
             value={newBudget}
             onChange={(e) => setNewBudget(parseFloat(e.target.value))}
           />
-          <button onClick={handleIncrease}>Increase by 10</button>
-          <button onClick={handleDecrease}>Decrease by 10</button>
-          <button onClick={handleSave}>Save</button>
         </div>
       ) : (
-        <button onClick={() => setEditable(true)}>Edit Budget</button>
+        <label>
+          {currency} {budget}
+        </label>
+      )}
+
+      {editable && (
+        <div>
+          <button onClick={handleSave}>Save</button>
+        </div>
+      )}
+
+      {!editable && (
+        <div>
+          <button onClick={() => setEditable(true)}>Edit Budget</button>
+        </div>
       )}
     </div>
   );
